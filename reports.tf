6f4957dc87b913722ea7b3835837142661a848a9
Gareth Denny <37297485+gpdenny@users.noreply.github.com>
2024-01-03 11:34:06 +0000

    feat: add backup reports feature

---
+++ reports.tf
@@ -0,0 +1,24 @@
+resource "aws_backup_report_plan" "ab_report" {
+  for_each = var.backup_reports
+
+  name        = each.value.name
+  description = each.value.description
+
+  report_delivery_channel {
+    formats        = each.value.formats
+    s3_bucket_name = each.value.s3_bucket_name
+    s3_key_prefix  = each.value.s3_key_prefix
+  }
+
+  report_setting {
+    report_template      = each.value.report_template
+    accounts             = each.value.accounts
+    organization_units   = each.value.organization_units
+    regions              = each.value.regions
+    framework_arns       = each.value.framework_arns
+    number_of_frameworks = length(each.value.framework_arns)
+  }
+
+  tags = merge(var.tags, { Name = each.value.name })
+
+}
